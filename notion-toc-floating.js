fc42444398c8f995ef7676459e992b1b219c94c8
gefangshuai <gefangshuai@163.com>
2021-05-25 18:30:38 +0800

    coding

--- notion-toc-floating.js
+++ notion-toc-floating.js
@@ -1,33 +1,55 @@
 // ==UserScript==
 // @name         Notion TOC Floating
 // @namespace    https://github.com/gefangshuai
-// @version      0.1
+// @version      0.1.1
 // @description  mark your toc floating! 默认取第一个table of contents，请知晓。
 // @author       Timothy.Ge
 // @include      *://*.notion.so/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @note         21-05-25 0.1.1 解决了更换文章失效的问题
 // @note         21-05-25 0.1.0 第一个测试版本
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    // Your code here...
-    document.querySelector('.notion-scroller').addEventListener('scroll', function(e) {
-        var tableContents = document.querySelector('.notion-table_of_contents-block');
-        if (tableContents) {
-            var rect = tableContents.getBoundingClientRect();
-            var dom = tableContents.firstChild
-            if (dom) {
-                console.log(rect.top);
-                if (rect.top <= 60) {
-                    dom.style.position = 'fixed';
-                    dom.style.top = '60px';
-                }else{
-                    dom.style.position = null;
-                    dom.style.top = null;
+    (function(history) {
+        'use strict';
+        // Your code here...
+        var doSomething = function() {
+            var scroller = document.querySelector('div.notion-frame .notion-scroller');
+            var scrollWatchFun = function(e) {
+                var tableContents = document.querySelector('.notion-table_of_contents-block');
+                if (tableContents) {
+                    var rect = tableContents.getBoundingClientRect();
+                    var dom = tableContents.firstChild;
+                    if (dom) {
+                        if (rect.top <= 60) {
+                            dom.style.position = 'fixed';
+                            dom.style.top = '60px';
+                        } else {
+                            dom.style.position = null;
+                            dom.style.top = null;
+                        }
+                    }
                 }
+            };
+            if (scroller) {
+                scroller.removeEventListener('scroll', scrollWatchFun);
+                scroller.addEventListener('scroll', scrollWatchFun);
             }
-        }
-    });
+        };
+        doSomething();
+        var pushState = history.pushState;
+        history.pushState = function(state) {
+            if (typeof history.onpushstate == 'function') {
+                history.onpushstate({ state: state });
+            }
+            setTimeout(function() {
+                doSomething();
+            }, 1000);
+            // ... whatever else you want to do
+            // maybe call onhashchange e.handler
+            return pushState.apply(history, arguments);
+        };
+    })(window.history);
 })();
