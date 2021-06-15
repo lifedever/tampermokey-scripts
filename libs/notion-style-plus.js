18c08f083319cf10c78d1150461b10dedb31e787
gefangshuai <gefangshuai@163.com>
2021-06-15 12:54:15 +0800

    update

--- libs/notion-style-plus.js
+++ libs/notion-style-plus.js
@@ -1,14 +1,19 @@
 // ==UserScript==
 // @name         Notion Style Plus | 重定义Notion样式：Quota
 // @namespace    https://github.com/gefangshuai/tampermokey-scripts
-// @version      1.0.0
+// @version      1.0.1
 // @author       Timothy.Ge
 // @include      *://*.notion.so/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
+// @note         21-06-15 1.0.1 修改Quote样式
 // @note         21-06-15 1.0.0 修改Quote样式
 // ==/UserScript==
-GM_addStyle(`
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
     .notion-quote-block div[contenteditable] {
         border-left: 4px solid #ddd !important;
     }
@@ -16,3 +21,4 @@ GM_addStyle(`
         font-size: 1em !important;
     }
 `);
+})
