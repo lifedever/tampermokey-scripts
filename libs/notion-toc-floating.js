6471f2f908b00d0386757ad7c64a0b54e07b7249
gefangshuai <gefangshuai@163.com>
2021-08-11 18:40:46 +0800

    update

--- libs/notion-toc-floating.js
+++ libs/notion-toc-floating.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Notion TOC Floating | 悬浮目录 | 悬浮Heading
 // @namespace    https://github.com/gefangshuai/tampermokey-scripts
-// @version      0.1.7
+// @version      0.1.8
 // @description  默认取第一个table of contents，请知晓。支持悬浮目录和悬浮Heading！
 // @author       Timothy.Ge
 // @include      *://*.notion.*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
-// @note         21-08-11 0.1.17 兼容新站点
-// @note         21-08-11 0.1.16 兼容新站点
+// @note         21-08-11 0.1.8 兼容新站点
+// @note         21-08-11 0.1.7 兼容新站点
+// @note         21-08-11 0.1.6 兼容新站点
 // @note         21-06-02 0.1.5 优化Sticky Heading逻辑，当页面包含多列的时候，禁用悬浮
 // @note         21-06-02 0.1.4 优化了悬浮逻辑，增加了table_of_contents激活效果
 // @note         21-06-02 0.1.3 新增了对h1、h2、h3的悬浮支持
